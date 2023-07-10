36e4f398d186da7a36722d14e7131ab811045d09
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2023-07-11 03:52:17 +0530

    Time: 3 ms (79.62%), Space: 15.7 MB (24.74%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    void solve(int c1, int c2, string s, vector<string>& ans){
+        if(c1 == 0 && c2 == 0 ){
+            ans.push_back(s);
+            return;
+        }
+
+        if(c1 == c2){
+            string s1 = s;
+            s1.push_back('(');
+            solve(c1-1,c2,s1,ans);
+        }
+
+        else if(c1 == 0){
+            string s1 = s;
+            s1.push_back(')');
+            solve(c1,c2-1,s1,ans);
+        }
+
+        else if(c2 == 0){
+            string s1 = s;
+            s1.push_back('(');
+            solve(c1-1,c2,s1,ans);
+        }
+
+        else {
+            string s1 = s,s2 = s;
+            s1.push_back('(');
+            s2.push_back(')');
+            solve(c1-1,c2,s1,ans);
+            solve(c1,c2-1,s2,ans);
+        }
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string  s ="";
+        solve(n,n,s,ans);
+        return ans;
+    }
+};
