86f8e82a0192e3052d3aaf259e37c643bad24bc3
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2023-08-02 22:32:47 +0530

    Time: 0 ms (100%), Space: 7.4 MB (99.75%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
 
-    void recr(int i,vector<vector<int>>& ans,vector<int>& nums){
-        if(i==nums.size()){
+    int recr(int i){
+        if( i <= 1) return 1;
+        return i*recr(i-1);
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int n = nums.size();
+
+        int i = recr(n);
+        vector<vector<int>> ans;
+
+        for(int j=0;j<i;j++){
             ans.push_back(nums);
-            return;
+            next_permutation(nums.begin(),nums.end());
         }
 
-        for(int ind = i;ind<nums.size();ind++){
-            swap(nums[i],nums[ind]);
-            recr(i+1,ans,nums);
-            swap(nums[i],nums[ind]);
-        }
-        
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>  ans;
-        recr(0,ans,nums);
-        // sort(ans.begin(),ans.end());
         return ans;
     }
 };
