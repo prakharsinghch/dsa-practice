b030acf19333a18c98177a21f5620d41c8730de9
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2023-08-13 14:17:38 +0530

    Time: 812 ms (10%), Space: 232.7 MB (10%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     
-   
+   bool check(vector<int>& nums, int m, int x){
+       int n = nums.size();
+       set<int> s;
+       for(int i=n-x-1;i>=0;i--){
+           s.insert(nums[i+x]);
+           auto j = s.lower_bound(nums[i]);
+
+           if(abs(*j-nums[i]) <= m) return true;
+
+            if(j != s.begin()){
+                j--;
+                if(abs(*j-nums[i])<= m) return true;
+            }
+       }
+
+       return false;
+   }
     
     int minAbsoluteDifference(vector<int>& nums, int x) {
 
-        multiset<int> m;
-        int ans = INT_MAX;
-        for(int i  = 0;i<nums.size();i++){
-            if(i-x >= 0) m.insert(nums[i-x]);
-            auto c = m.upper_bound(nums[i]);
+        int mx = *max_element(nums.begin(),nums.end());
+        int mn = *min_element(nums.begin(),nums.end());
+        
+        int l = 0, r = mx-mn;
+        int res = 0;
+        while(l<=r){
+            int m =  l + (r - l) / 2;
 
-            if(c != m.end()) ans = min(ans, abs(nums[i] - *c));
-            if (c!= m.begin())ans = min(ans, abs(nums[i] - *prev(c)));
+            if(check(nums,m,x)){
+                r = m-1;
+                res = m;
+            }
+            else l = m+1;
         }
 
-        return ans;    
+        return res;
 
-        
-        
     }
 };
