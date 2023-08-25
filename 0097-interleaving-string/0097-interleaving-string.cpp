8169e4d0fe8e6002cdcf112d29fa2b9382a47355
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2023-08-25 13:08:51 +0530

    Time: 20 ms (37.13%), Space: 25.9 MB (28.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    // int dp[102][102];
-    bool helper(string& s1, string& s2, string &s3, int l1, int l2, int l3, vector<vector<int>>&dp) {
-        if(l1<0 && l2<0 && l3<0) {
-            return true;
+
+    bool recr(string s1, string s2, string s3, int i, int j, int k,vector<vector<int>>& dp){
+        if(k == s3.size() && i == s1.size() && j == s2.size()) return true;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i < s1.size() && s1[i] == s3[k] && j< s2.size() && s2[j] == s3[k]){
+            return dp[i][j] = (recr(s1,s2,s3,i+1,j,k+1,dp) || recr(s1,s2,s3,i,j+1,k+1,dp));
         }
-        if(l1>=0 && l2>=0 && dp[l1][l2]!=-1) {
-            return dp[l1][l2];
+
+        else if(i < s1.size() && s1[i] == s3[k]){
+            return dp[i][j] = (recr(s1,s2,s3,i+1,j,k+1,dp));
         }
-        if(l1>=0 && s1[l1]==s3[l3] && l2>=0 && s2[l2]==s3[l3] ) {
-            return dp[l1][l2] = (helper(s1, s2, s3, l1-1, l2, l3-1, dp) || helper(s1, s2, s3, l1, l2-1, l3-1, dp));
+
+        else if(j < s2.size() && s2[j] == s3[k]){
+            return dp[i][j] = recr(s1,s2,s3,i,j+1,k+1,dp);
         }
-        else if(l1>=0 && s1[l1]==s3[l3])
-            return helper(s1, s2, s3, l1-1, l2, l3-1, dp);
-        
-        else if(l2>=0 &&s2[l2]==s3[l3])
-            return helper(s1, s2, s3, l1, l2-1, l3-1, dp);
-        
-        else return false;
+
+
+        return false;
+
     }
-    
+
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.size() != s1.size() + s2.size())
-			return false;
-        vector<vector<int>>dp(s1.size()+1, (vector<int>(s2.size()+1, -1)));
-        // memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, s3,s1.size()-1, s2.size()-1, s3.size()-1, dp);
+        if(s3.size() != s1.size() + s2.size()) return false;
+        vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1,-1));
+        return recr(s1,s2,s3,0,0,0,dp);
     }
 };
