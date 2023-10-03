15b361558402d82b181743b2decd72e26ca6877f
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2023-10-03 19:37:29 +0530

    Time: 3 ms (42.92%), Space: 7.5 MB (16.85%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto x: nums){
+            mp[x]++;
+        }
+
+        int ans = 0;
+        for(auto x: mp){
+            if(x.second == 1) continue;
+            else if(x.second == 2) ans++;
+            else ans+=(x.second-1)*x.second/2;
+        }
+
+        return ans;
+    }
+};
