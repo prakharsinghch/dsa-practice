ca5f1b98ef6d57d77ef8f2be5041e826a26cae23
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2025-01-28 16:19:34 +0530

    Time: 0 ms (100%), Space: 9.3 MB (87.59%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,29 +1,32 @@
 class MyStack {
 public:
-    vector<int> v;
+
+    queue<int> q;
+
     MyStack() {
         
     }
     
     void push(int x) {
-        v.push_back(x);
-        
+        q.push(x);
+        for(int i=0;i<q.size()-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
     }
     
     int pop() {
-        int n = v [ v.size() -1];
-        v.resize(v.size()-1);
-        return n;
-        
+        int x = q.front();
+        q.pop();
+        return x;
     }
     
     int top() {
-        return v[v.size() -1];
+        return q.front();
     }
     
     bool empty() {
-        if(v.size()) return false;
-        else return true;
+        return q.empty();
     }
 };
 
