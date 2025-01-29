5d80c266d9a968cfbbef318f7465b83767620a85
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2025-01-29 16:01:29 +0530

    Time: 14 ms (10.79%), Space: 18.4 MB (9.09%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,52 +1,32 @@
 class Solution {
-    vector<int> ans;
-    int flag =1;
-    void BFS(int src, int des, vector<vector<int>> &graph,vector<int> &visited)  {
-        queue<int> q;
-        q.push(src);
-        
-        while(!q.empty())  {
-            int top = q.front();
-            q.pop();
-            if(visited[top])
-                return;
-            visited[top] = 1;
-            
-            if(top == des)  {                
-                ans = {src, des};
-                flag = 0;
-                return;
-            }
-            
-            for(auto &i: graph[top])  {
-                if(visited[i]==0)  {
-                    if(top==src && i==des)
-                        continue;
-                    else
-                        q.push(i);
-                }
-            }
-        }
-        return;
-    }
 public:
+
+    bool check(unordered_map<int,vector<int>>& mp, int u, int v){
+        set<int> vis;
+        stack<int> s;
+        s.push(u);
+        while(!s.empty()){
+            int x = s.top();
+            s.pop();
+            if(vis.count(x)) continue;
+            vis.insert(x);
+            if(x == v) return true;
+            for(auto y:mp[x]) s.push(y);
+        }
+
+        return false;
+    }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<vector<int>> graph(n+1);
-        
-        for(int i=0; i<edges.size(); ++i)  {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+        unordered_map<int, vector<int>> mp;
+        for(auto x : edges){
+            int u = x[0], v =x[1];
+            if(mp.count(u) && mp.count(v) && check(mp,u,v)) return x; 
+
+            mp[u].push_back(v);
+            mp[v].push_back(u);
         }
-        
-        
-        for(int i=n-1; i>=0; --i)  {
-            if(flag)  {
-                vector<int> visited(n+1,0);
-                BFS(edges[i][0],edges[i][1],graph, visited);    
-            }            
-        }
-        
-        return ans;
+
+        return {};
     }
 };
