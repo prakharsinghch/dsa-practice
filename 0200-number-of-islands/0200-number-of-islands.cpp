56915c66ebb11beea66e5775fcad34bba2de0b40
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2025-02-13 23:53:27 +0530

    Time: 24 ms (78.33%), Space: 18.3 MB (42.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-
-    void recr(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || vis[i][j] == 1 || grid[i][j] == '0' ) return;
-
-        vis[i][j] = 1;
-        recr(grid,vis,i-1,j);
-        recr(grid,vis,i+1,j);
-        recr(grid,vis,i,j-1);
-        recr(grid,vis,i,j+1);
-    }
-
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size(), n=grid[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        int ans =0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == '1' && vis[i][j] == 0 ){
-                    ans++;
-                    recr(grid,vis,i,j);
-                } 
-            }
-        }
+        if(grid.empty() || grid[0].empty()) return 0;
 
-        return ans;
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dir = {0,-1,0,1,0};
+
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto [x,y] = q.front();
+                        q.pop();
+                        if(x < 0 || y <0 || x >=m || y >= n || grid[x][y] != '1') continue;
+
+                        grid[x][y] = 0;
+                        for(int i=1;i<dir.size();i++){
+                            int rx = x + dir[i-1], ry = y + dir[i];
+                            if(rx < 0 || ry <0 || rx >=m || ry >= n || grid[rx][ry] != '1') continue;
+                            else{
+                                q.push({rx,ry});
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+         return ans;
     }
 };
