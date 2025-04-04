84d553811c21825b0d64cc9e55d5613e77127131
Prakhar Singh <prakharsinghchouhan21@gmail.com>
2025-04-04 22:26:55 +0530

    Time: 21 ms (16.47%), Space: 18.7 MB (16.24%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
 
-    bool recr(string s, set<string> t,int start){
-        if(start == s.length()) return true;
-
-        for(int i=start;i<s.length();i++){
+    bool recr(string s,set<string>& st,int start,vector<int>& dp){
+        if(start == s.size()) return true;
+        if(dp[start] != -1) return dp[start];
+        for(int i=start;i<s.size();i++){
             string s1 = s.substr(start,i+1-start);
-            if(t.count(s1) && recr(s,t,i+1) ) return true;
+            if(st.count(s1) && recr(s,st,i+1,dp)) return dp[start] = true;
         }
-        
-        return false;
 
+        return dp[start] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> t;
-        
-        for(auto x: wordDict) t.insert(x);
+        set<string> st;
+        vector<int> dp(s.size()+1,-1);
+        for(auto x: wordDict) st.insert(x);
 
-        return recr(s,t,0);
+        return recr(s,st,0,dp);
+
     }
-
 };
